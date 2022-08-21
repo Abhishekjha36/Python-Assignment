d887bda3dd2bfe38199200ef6be2bd154bc6900d
Abhishek jha <111429889+Abhishekjha36@users.noreply.github.com>
2022-08-21 14:36:05 +0530

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,59 +1,50 @@
 
 # Q1
-"""
+
 a="5"
 print(type(a))
-"""
+
 # Q2
-"""
+
 a="m"
 print(ord(a))
-"""
+
 # Q3
-"""
 a=100
 print(chr(a))
-"""
+
 # Q4
-"""
 a=58
 print(bin(a))
-"""
+
 
 # Q5
-"""
 a=25
 print(oct(a))
-"""
-
 
 # Q6
-"""
 a=39
 print(hex(a))
-"""
+
 # Q7
-"""
 x="0b1100101"
 dec_x=int(x, 2)
 print(dec_x)
-"""
+
 # Q8
-"""
+
 a=0x2F
 print(oct(a))
-"""
+
 
 # Q9
-"""
+
 a=0o125
 print(bin(a))
-"""
+
 
 #Q10
-"""
 a=0o31
 b=0x27
 c=a+b
 print(bin(c))
-"""
