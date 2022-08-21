feda1e8b36513e853390a85365a683cdfb9cffb4
Abhishek jha <111429889+Abhishekjha36@users.noreply.github.com>
2022-08-21 13:49:41 +0530

    Add files via upload

---
+++ Assigment1,2.py
@@ -0,0 +1,84 @@
+#Q4
+###print("Hello Python")
+"""
+print("Hello ")
+
+print("Python")
+
+print(""" "MySirG" """)
+
+print(""" "Teacher's Day" """)
+
+x="Abhishek jha"
+print(x)
+ """
+
+
+
+""""
+name="Name Abhishek Jha" 
+age="age 19"
+qual="qualification 12th pass"
+expe="experience of 2 years"
+print(name,age,qual,expe,sep="\n")
+"""
+
+a=5
+b=7
+c=8
+print(a,b,c,sep=("\n"))
+
+
+#print('Learning Python')
+
+""""name="abhishek jha"
+x="mysirg"
+a=7
+b=8
+print(name,x,a,b,sep=("\n"))
+"""
+
+"""
+a=35
+b=True
+c="MySirG"
+d=5.46
+e=3+4j
+print(type(a),type(b),type(c),
+      type(d),type(e),sep=("\n"))
+"""
+
+
+"""
+x=45
+y=46
+print(id(x),id(y),sep=("\n"))
+"""
+
+"""
+a=23
+b=False
+c=76.7
+d='MySirG'
+print(type(a),type(b),type(c),type(d),sep=("\n"))
+print(id(a),id(b),id(c),id(d),sep=("\n"))
+
+"""
+
+"""
+import keyword
+print(keyword.kwlist)
+
+"""
+
+"""
+A=True
+print(A)
+"""
+
+
+"""
+date=("17-8-2022")
+time=("1:00 PM")
+print(date,time,sep=("\n"))
+"""
